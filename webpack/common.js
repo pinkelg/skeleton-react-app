84deda8085ae06b4fd359fa40f60bce685304d21
pinkelg <97774000+pinkelg@users.noreply.github.com>
2022-12-19 00:17:03 -0500

    Phase-1: initial commit (#1)

---
+++ webpack/common.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  resolve: { extensions: [".tsx", ".ts", ".js"] },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.(tsx|mjs|jsx|ts|js)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "../public"),
+          globOptions: {
+            ignore: ["**/*.html"],
+          },
+          to: "./",
+        },
+      ],
+    }),
+  ],
+};
