95023475cdbe9e9e92045cd0ee066e780ba6caa4
Pinkel Gurung <pinkelgurung@gmail.com>
2022-12-19 23:21:54 -0500

    phase-2

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+const prettierConfig = require("./prettier.config");
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  parser: "@typescript-eslint/parser",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: ["./tsconfig.json"]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"]
+      }
+    }
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": ["error", prettierConfig]
+  }
+};
